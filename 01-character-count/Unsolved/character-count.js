27c88792c51c366f55b035c55acad5b0c2621fa4
Gordon Karlsson <114494147+gpkarlsson@users.noreply.github.com>
2023-02-21 14:58:27 -0600

    Add files via upload

---
+++ 01-character-count/Unsolved/character-count.js
@@ -0,0 +1,6 @@
+// Write code to create a function that accepts a string and returns an object containing the number of times each character appears in the string
+
+var characterCount = function(str) {
+//  const textArray = str.split('');
+//  console.log(textArray);
+};
